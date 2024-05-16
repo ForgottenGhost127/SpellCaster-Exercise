81e5e7ddb1ad60da851b33ee70d5385ec2b3cab3
LAPTOP-0CENV2U2\jufla <judith.legaz@hotmail.com>
2024-05-17 00:59:42 +0200

    Little Update

--- Assets/Scripts/LibroHechizos.cs
+++ Assets/Scripts/LibroHechizos.cs
@@ -7,12 +7,19 @@ public class LibroHechizos : MonoBehaviour
     //Attributes
     public List<string> hechizosLearned;
 
+    private bool libroAbi = false;
+
     //Methods
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Q))
         {
             MostrarHechizosAp();
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            CerrarLibro();
         }
     }
     public void AprenderHechizo(string hechizoName)
@@ -21,6 +28,22 @@ public class LibroHechizos : MonoBehaviour
     }
     public void MostrarHechizosAp()
     {
-        print("Has abierto el libro");
+       if(!libroAbi)
+        {
+            libroAbi = true;
+            print("Has abierto el libro");
+        }
+        
     }
+
+    public void CerrarLibro()
+    {
+        if(libroAbi)
+        {
+            libroAbi = false;
+            print("Has cerrado el libro");
+        }
+    }
+
+   
 }
